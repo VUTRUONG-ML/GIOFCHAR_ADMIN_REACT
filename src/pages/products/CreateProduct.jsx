1f09b00b4262c994772afc5c83b38fff06bcd4ad
LE BAR <letruong8017@gmail.com>
2025-12-05 21:52:06 +0700

    upload image into create product page

--- src/pages/products/CreateProduct.jsx
+++ src/pages/products/CreateProduct.jsx
@@ -1,24 +1,66 @@
+import { useRef, useState } from "react";
 import { InputCreate } from "../../components/InputCreate";
 import { SubTitle } from "../../components/SubTitle";
 import { TextArea } from "../../components/TextArea";
 import { VND } from "../../constants/currency";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
+import { toast } from "react-toastify";
 export default function CreateProduct() {
+  const [linkImage, setLinkImage] = useState(null);
+  const fileInputRef = useRef(null);
+  const handleOpenFolder = () => fileInputRef.current.click();
+  const handleFileChange = (e) => {
+    console.log(e.target);
+    const file = e.target.files[0];
+    if (!file) return;
+
+    if (file.size > 5 * 1024 * 1024) {
+      // switch byte 1 MB = 1024 * 1024 byte
+      toast.warn("Vui lòng chọn ảnh có kích thước bé hơn!");
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setLinkImage(url);
+  };
   return (
     <>
       {/*subTitle */}
       <SubTitle title="Thêm sản phấm mới" />
       {/* data */}
       <div className="flex-1 flex gap-4 py-1">
-        <div className="flex-3 flex flex-col gap-3 bg-white shadow rounded-xl p-4">
-          <div className="w-full flex-1 flex flex-col items-center justify-center gap-2 border-2 border-dashed border-gray-300 rounded-2xl cursor-pointer text-primary">
-            <div className="">
-              <FileUploadOutlinedIcon sx={{ fontSize: 80 }} />
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-md font-medium ">Nhấn để tải ảnh sản phẩm</p>
-              <p className="text-sm ">PNG, JPG tối đa 5MB</p>
-            </div>
+        <div className="flex-3 flex flex-col gap-3 bg-white shadow rounded-xl p-4 items-center">
+          {/* uploadFile */}
+          <div
+            className="w-[80%] flex-1 flex flex-col items-center justify-center gap-2 border-2 border-dashed border-gray-300 rounded-2xl cursor-pointer text-primary"
+            onClick={handleOpenFolder}
+          >
+            {!linkImage ? (
+              <>
+                <div className="">
+                  <FileUploadOutlinedIcon sx={{ fontSize: 80 }} />
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-md font-medium ">
+                    Nhấn để tải ảnh sản phẩm
+                  </p>
+                  <p className="text-sm ">PNG, JPG tối đa 5MB</p>
+                </div>
+              </>
+            ) : (
+              <img
+                src={linkImage}
+                className=" w-fit max-h-80  rounded-2xl object-cover"
+              />
+            )}
+
+            <input
+              type="file"
+              className=" hidden"
+              ref={fileInputRef}
+              accept="image/png, image/jpeg"
+              onChange={handleFileChange}
+            />
           </div>
           <div className="flex items-center w-full gap-2">
             {/* name */}
